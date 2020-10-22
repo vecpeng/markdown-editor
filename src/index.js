de5417e7444c81c668f04a5b9ce3ffb1816480e0
vecpeng <841298391@qq.com>
2020-10-22 23:12:23 +0800

    feature:1-6title

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Container extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: []
+    }
+    this.handleChildInput = this.handleChildInput.bind(this)
+  }
+  handleChildInput(userInput) {
+    this.setState({
+      value: userInput
+    })
+    console.log(userInput)
+  }
   render() {
     return(
       <div className = "container">
-        <UserInput />
+        <UserInput handleChildInput = {this.handleChildInput}/>
+        <UserOutput value = {this.state.value}/>
       </div>
     )
   }
@@ -15,15 +29,25 @@ class UserInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: []
     }
-    this.handleValue = this.handleValue.bind(this)
+    this.handleInput = this.handleInput.bind(this)
   }
-  handleValue(e) {
-    this.setState({
-      value: e.target.value
+  handleInput(e) {
+    console.log("ok")
+    console.log(this.props)
+    const {handleChildInput} = this.props
+    console.log(handleChildInput)
+    let userInput = document.getElementById("user-input")
+    console.log(userInput.textContent)
+    let handleInput = [userInput.innerHTML.split("<div>")[0]]
+    console.log(userInput)
+    Array.from(userInput.childNodes).forEach(childNode => {
+      if(childNode.nodeName === 'DIV') {
+        handleInput.push(childNode.textContent)
+      }
     })
-    console.log(this.state.value)
+    handleChildInput(handleInput)
   }
   handleKeyDown(e) {
     if(e.keyCode === 13) {
@@ -36,11 +60,44 @@ class UserInput extends React.Component {
     return (
       <div 
       contentEditable = "true"
+      spellCheck = "false"
       className = "user-input"
+      id = "user-input"
+      onInput = {this.handleInput}
       >
       </div>
     )
   }
 }
 
+class UserOutput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ""
+    }
+  }
+  render() {
+    console.log(this.props)
+    const output = this.props.value.map((value,index) => {
+      // if(value.slice(0,1) ===  "###") {
+      //   return <h3 key={index}>{value.slice(3)}</h3>
+      // }
+      // return value
+      let num = 0
+      for(let i = 0; i < 6; i++) {
+        if(value[i] === '#'){
+          num++
+        } else {
+          break
+        }
+      }
+      const Tag = "h" + num
+      return <Tag >{value.slice(num)}</Tag>
+    })
+    return (
+    <div>{output}</div>
+    )
+  }
+}
 ReactDOM.render(<Container />, document.getElementById("root"))
